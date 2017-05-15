1e4313e65c9620210d7fd555a8bee985bec6dbb2
Fank <mengfanyu81@sina.com>
2017-05-15 09:56:44 +0800

    pcntl_signal
    sms  信号处理

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-echo 'Hello World!';
+function Has2Base64(){
+	// 信号处理
+		pcntl_signal( SIGTERM, array( __CLASS__,"signalHandler" ), false );
+		pcntl_signal( SIGINT, array( __CLASS__,"signalHandler" ), false );
+		pcntl_signal( SIGQUIT, array( __CLASS__,"signalHandler" ), false );
+		pcntl_signal( SIGHUP, array( __CLASS__, "signalHandler" ), false );
+		pcntl_signal( SIGTSTP, array( __CLASS__, "signalHandler" ), false );
+}
