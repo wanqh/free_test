6a5bcaf8dd3987efa2d69c28f87b65dbc624de8b
Fank <mengfanyu81@sina.com>
2017-05-16 20:13:49 +0800

    Revert "pcntl_signal"
    This reverts commit 1e4313e65c9620210d7fd555a8bee985bec6dbb2.

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
 <?php
-function Has2Base64(){
-	// 信号处理
-		pcntl_signal( SIGTERM, array( __CLASS__,"signalHandler" ), false );
-		pcntl_signal( SIGINT, array( __CLASS__,"signalHandler" ), false );
-		pcntl_signal( SIGQUIT, array( __CLASS__,"signalHandler" ), false );
-		pcntl_signal( SIGHUP, array( __CLASS__, "signalHandler" ), false );
-		pcntl_signal( SIGTSTP, array( __CLASS__, "signalHandler" ), false );
-}
+echo 'Hello World!';
