f7b27cb044f9e3e80f55f2559748f98f670081a4
root <mengfanyu81@sina.com>
2017-05-25 01:18:45 +0800

    pcntl

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
-echo 'Hello World!';
-echo '8888';
+//
+//test pcntl 
+$pid=pcntl_fork();
+switch($pid){
+
+case-1:
+
+    echo"couldn't fork";
+
+    break;
+
+case0:
+
+    echo"I'm parent";
+
+    break;
+
+default:
+
+    echo"I'm child";
+
+}
+
+?>
